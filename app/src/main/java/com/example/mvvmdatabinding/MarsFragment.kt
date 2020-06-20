435cde67e21d611e5d43760339f17af8ab327d58
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-20 18:21:06 +0530

    created viewModel instance in Fragment

--- app/src/main/java/com/example/mvvmdatabinding/MarsFragment.kt
+++ app/src/main/java/com/example/mvvmdatabinding/MarsFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 
 class MarsFragment : Fragment() {
 
+    private lateinit var factory: MarsViewModelFactory
     private lateinit var viewModel: MarsViewModel
 
     override fun onCreateView(
@@ -18,5 +20,13 @@ class MarsFragment : Fragment() {
         return inflater.inflate(R.layout.mars_fragment, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        val api = MarsApi()
+        val repository = MarsRepository(api)
+        factory = MarsViewModelFactory(repository)
+
+        viewModel = ViewModelProvider(this, factory).get(MarsViewModel::class.java)
+    }
 }
