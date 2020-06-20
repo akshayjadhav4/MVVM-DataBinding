499380cad52d6aaf63318b4204af10258e9c326e
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-20 16:57:45 +0530

    showed API response in Toast

--- app/src/main/java/com/example/mvvmdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdatabinding/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.mvvmdatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = MarsRepository(MarsApi())
+        GlobalScope.launch(Dispatchers.Main) {
+            val properties = repository.getMarsProperties()
+            Toast.makeText(this@MainActivity, properties.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
