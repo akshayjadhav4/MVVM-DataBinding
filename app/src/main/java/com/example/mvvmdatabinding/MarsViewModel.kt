6901779ff12cd794f9fc3dfe69ae949f0ea0885f
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-20 18:08:19 +0530

    defined live data

--- app/src/main/java/com/example/mvvmdatabinding/MarsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabinding/MarsViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.mvvmdatabinding
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MarsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MarsViewModel(private val repository: MarsRepository) : ViewModel() {
+
+    //Live data
+    private val _marsProperties = MutableLiveData<List<MarsProperty>>()
+    val marsProperties: LiveData<List<MarsProperty>>
+        get() = _marsProperties
+
 }
