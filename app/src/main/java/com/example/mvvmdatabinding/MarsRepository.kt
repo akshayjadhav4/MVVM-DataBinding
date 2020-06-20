9a81f49ddbe4d252c3c961f2011a1b5273e31cbc
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-20 15:27:28 +0530

    created repository  for mars properties

---
+++ app/src/main/java/com/example/mvvmdatabinding/MarsRepository.kt
@@ -0,0 +1,6 @@
+package com.example.mvvmdatabinding
+
+class MarsRepository(private val api: MarsApi) : SaveApiRequest() {
+
+    suspend fun getMarsProperties() = ApiRequest { api.getProperties() }
+}
