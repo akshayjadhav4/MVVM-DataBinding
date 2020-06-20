cdfe18398a42a0ae411bc91b5376f145bda4f44e
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-20 15:16:53 +0530

    defined retrofit to call API endpoint

---
+++ app/src/main/java/com/example/mvvmdatabinding/MarsApi.kt
@@ -0,0 +1,21 @@
+package com.example.mvvmdatabinding
+
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface MarsApi {
+
+    @GET("realestate")
+    suspend fun getProperties(): Response<List<MarsProperty>>
+
+    companion object {
+        operator fun invoke(): MarsApi {
+            return Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://mars.udacity.com/")
+                .build()
+                .create(MarsApi::class.java)
+        }
+    }
+}
