6537243686af5a38d852ca3da3e4abe025f7d2dd
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-21 15:45:45 +0530

    Created Adapter for Mars

--- app/src/main/java/com/example/mvvmdatabinding/MarsAdapter.kt
+++ app/src/main/java/com/example/mvvmdatabinding/MarsAdapter.kt
@@ -1,7 +1,30 @@
 package com.example.mvvmdatabinding
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mvvmdatabinding.databinding.RecyclerviewMarsBinding
 
-class MarsAdapter():RecyclerView.Adapter<>{
+class MarsAdapter(
+    private val marsProperties: List<MarsProperty>
+) : RecyclerView.Adapter<MarsAdapter.MarsViewHolder>() {
 
+    override fun getItemCount() = marsProperties.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MarsViewHolder(
+        DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.recyclerview_mars,
+            parent,
+            false
+        )
+    )
+
+    override fun onBindViewHolder(holder: MarsViewHolder, position: Int) {
+        holder.recyclerviewMarsBinding.mars = marsProperties[position]
+    }
+
+    inner class MarsViewHolder(val recyclerviewMarsBinding: RecyclerviewMarsBinding) :
+        RecyclerView.ViewHolder(recyclerviewMarsBinding.root)
 }
