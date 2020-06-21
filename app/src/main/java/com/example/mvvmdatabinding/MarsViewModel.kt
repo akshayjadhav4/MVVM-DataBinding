2357b2d7390a711685b930393942b115b3ff091f
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-21 13:27:06 +0530

    changes made in getProperties() to switch to IO thread

--- app/src/main/java/com/example/mvvmdatabinding/MarsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabinding/MarsViewModel.kt
@@ -3,16 +3,21 @@ package com.example.mvvmdatabinding
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
 
 class MarsViewModel(private val repository: MarsRepository) : ViewModel() {
+
+    private lateinit var job: Job
 
     //Live data
     private val _marsProperties = MutableLiveData<List<MarsProperty>>()
     val marsProperties: LiveData<List<MarsProperty>>
         get() = _marsProperties
 
-    suspend fun getMarsProperties() {
-        val properties = repository.getMarsProperties()
-        _marsProperties.value = properties
+    fun getMarsProperties() {
+        job = Coroutines.ioThenMain(
+            { repository.getMarsProperties() },
+            { _marsProperties.value = it }
+        )
     }
 }
