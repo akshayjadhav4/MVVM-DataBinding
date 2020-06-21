eb5f0ccc0dada545f71927aa2b2ba71d49416fae
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-21 18:59:37 +0530

    comment added

--- app/src/main/java/com/example/mvvmdatabinding/BindingUtil.kt
+++ app/src/main/java/com/example/mvvmdatabinding/BindingUtil.kt
@@ -4,7 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-
+//image value passing in pram is used in xml
 @BindingAdapter("image")
 fun loadImage(view: ImageView, url: String) {
     Glide.with(view).load(url).into(view)
